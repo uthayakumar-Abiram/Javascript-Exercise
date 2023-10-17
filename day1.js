61605b6bef4e00cc6a4d38fbe0e08aecfc928592
Abiram <uthayakumarabiram@gmail.com>
2023-10-17 15:28:05 +0530

    new

--- day1.js
+++ day1.js
@@ -1,10 +1,14 @@
-//hello//
-//_Welcome to learn Javascript_//
-/*_comments can make code readable, easy to reuse_
-_and informative*/
+git
 var x=12;
 var x="abi";
   //console.log(y);
 var y=true;
 var k=null;
 console.log(typeof y)
+console.log(typeof "vanakkam" );
+console.log(typeof 2 );
+console.log(typeof 1n );
+console.log(typeof function (){} );
+console.log(typeof null );
+console.log(typeof Symbol() );
+
